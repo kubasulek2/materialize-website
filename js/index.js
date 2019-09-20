c41b55be05a21188762399a7cb48db13137b1bf3
Kuba Sukowski <kubasulek2@gmail.com>
2019-09-20 19:43:35 +0200

    about in progress

--- js/index.js
+++ js/index.js
@@ -1,9 +1,17 @@
 'use strict';
-
+function get() {
+	
+}
 $(function () {
 	$('.sidenav').sidenav();
 	$('.fixed-action-btn').floatingActionButton({ hoverEnabled: false });
 	$('.modal').modal();
 	$('.scrollspy').scrollSpy();
+
+	$('.slider').slider({
+		transition: 500,
+		interval: 6000,
+		indicators: false
+	});
 
 });
