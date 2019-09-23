ca1b27612a4c9e2b9ed9c093ec33a8f8227a9d64
Kuba Sukowski <kubasulek2@gmail.com>
2019-09-23 18:33:01 +0200

    stats done, preloader done

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ $(function () {
 
 	$('.sidenav').sidenav();
 	$('.website-fixed.fixed-action-btn').floatingActionButton({ hoverEnabled: false });
-	
+
 	$('.admin .fixed-action-btn').floatingActionButton({ direction: 'left', hoverEnabled: false });
 
 
@@ -40,8 +40,54 @@ $(function () {
 	});
 
 	/* SCROLL TO TOP SMOOTH SCROLLING */
-	$('.scroll-top').click(function(){
+	$('.scroll-top').click(function () {
 		$('html, body').animate({ scrollTop: 0 }, 600);
 		return false;
 	});
+
+
+	/* ADMIN COUNTER UP */
+	$('.count').each(function () {
+		$(this).prop('Counter', 0).animate({ Counter: $(this).text() }, {
+			duration: 1000,
+			easing: 'swing',
+			queue: false,
+			step: function (now) {
+				$(this).text(Math.ceil(now));
+			}
+		});
+	});
+
+	
+
+	/* HIDE SECTION */
+
+	$('.admin .section').hide();
+	
+	setTimeout(() => {
+
+		/* HIDE PRELOADER */
+		$('.preloader-wrapper').hide();
+		/* SHOW SECTIONS */
+		$('.admin .section').fadeIn();
+
+		/* ADMIN COUNTER UP */
+		$('.count').each(function () {
+			$(this).prop('Counter', 0).animate({ Counter: $(this).text() }, {
+				duration: 1000,
+				easing: 'swing',
+				queue: false,
+				step: function (now) {
+					$(this).text(Math.ceil(now));
+				}
+			});
+		});
+
+		$('.determinate').each(function () {
+			$(this).animate({ width: $(this).data('width') }, { duration: 1500, queue: false });
+		});
+	}, 1000);
+
+
+
 });
